78f7f9b31a57aa00dc4ac117c147736702f67b91
nurselaine <Elaine.t.huynh@gmail.com>
2023-05-08 13:54:49 -0700

    Added styles and updated file structure

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+// // const io = require('socket.io-client');
+// import { io } from 'socket.io-client';
+// // let socket = io('https://localhost:3000');
+// let socket = io();
+
 const videoElem = document.getElementById("video");
 const startElem = document.getElementById("start");
 const stopElem = document.getElementById("stop");
+
+// const messages = document.getElementById('messages');
+// const form = document.getElementById('form');
+// const input = document.getElementById('input');
+
+// let username;
+// let connected = false;
+// let typing = flase;
+// let currentInput = input.focus();
+
+// // set client username
+// const addParticipantsMessage = (data) => {
+//   let message = '';
+//   if(data.numUsers === 1){
+//     message += 'there\'s 1 participant';
+//   } else {
+//     message += `there are ${data.numUsers} participants`;
+//   }
+//   console.log(message);
+// }
+
+// const setUsername = () => {
+//   username = cleanInput(input.val().trim());
+
+//   if(username) {
+//     socket.emit('add user', username);
+//   }
+// }
+
+// const sendMessage = () => {
+//   let message = input.val();
+//   message = cleanInput(message);
+//   if(message && connected){
+//     input.val('');
+//     socket.emit('new message', message);
+//   }
+// }
+
+//   // Prevents input from having injected markup
+//   const cleanInput = (input) => {
+//     return $('<div/>').text(input).html();
+//   }
 
 let displayMediaOptions = {
   video: {
@@ -25,6 +72,11 @@ async function startCapture(){
   try{
     // need help understanding this line
     videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    // navigator: this represents the browser
+    // mediaDevices: provides access to connected media input
+    // like screen sharing - lets you access any hardware source
+    // .getDisplayMedia() prompts user to select and grant
+    // 
   } catch (e){
     console.error(`Error has occured: ${e.message}`);
   }
